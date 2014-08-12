fc10b51ff02d8c3cbc58ec7e0f4a966e7f920d67
Mike Kibbel <mkibbel@gmail.com>
2014-08-12 10:26:59 -0400

    Upgraded path resolution for templates to use the npm glob library.

--- formidable.js
+++ formidable.js
@@ -1,21 +1,16 @@
 'use strict';
 
-// TEMP!
-var Formidable = function(base) {
-    var path,
-        urls;
+module.exports = function(options) {
+    var option = function(name) {
+            return options && options[name];
+        },
+        path = require('./lib/path')(option),
+        urls = require('./lib/urls')(path);
 
-    if (!(this instanceof Formidable)) {
-        return new Formidable(base);
-    }
-
-    path = require('./lib/path')(base || '.'),
-    urls = require('./lib/urls')(path),
-
-    this.path = path;
-    this.url = urls.url;
-    this.include = urls.include;
-    this.resolve = urls.resolve;
+    return {
+        path: path,
+        url: urls.url,
+        include: urls.include,
+        resolve: urls.resolve
+    };
 };
-
-module.exports = Formidable;
