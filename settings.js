ec481ab5ea9ac8b740fe5337418402cce0bc6409
Mike Kibbel <mkibbel@gmail.com>
2014-08-13 13:42:25 -0400

    Renamed lib/template to lib/templating to disambiguate the 'templates' option.

--- settings.js
+++ settings.js
@@ -76,7 +76,7 @@ var //---------------
             log = require('./lib/log')(option),
             path = require('./lib/path')(option),
             urls = require('./lib/urls')(option, path),
-            template = require('./lib/template/' + (option('templates') || 'swig'))(option, path, urls),
+            template = require('./lib/templating/' + (option('templating') || 'swig'))(option, path, urls),
             build = require('./lib/build')(option, log, path, urls, template);
 
         return extend(build, {
